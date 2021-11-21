5f83845051c42f4d0e1502f8cf3bb0addf6e01ab
Camilogonzalez10 <ccamilogonzalez1010@gmail.com>
2021-11-21 15:23:54 -0500

    Actividad 4

---
+++ Scrispts/Actividad.js
@@ -0,0 +1,169 @@
+$(".parrafo1").css ("background", "gray")
+$(".titulo").css ("background", "green")
+
+$(".pais1").click(
+function() { 
+
+    $(".pais1").css("background", "orange")
+}
+)
+
+$(".cambio1").dblclick(
+    function() { 
+    
+        $(".pais1").css("font-size", "35px")
+    }
+)
+
+$(".pais2").click(
+    function() { 
+    
+        $(".pais2").css("background", "gray")
+    }
+    )
+    
+    $(".cambio2").dblclick(
+        function() { 
+        
+            $(".pais2").css("font-size", "60px")
+        }
+    )
+
+
+    $(".pais3").mouseover(
+        function() { 
+        
+            $(".pais3").css("background", "yellow")
+        }
+        )
+        
+        $(".cambio3").dblclick(
+            function() { 
+            
+                $(".pais3").css("font-size", "23px")
+            }
+        )
+
+        $(".pais4").click(
+            function() { 
+            
+                $(".pais4").css("background", "purple")
+            }
+            )
+            
+            $(".cambio4").dblclick(
+                function() { 
+                
+                    $(".pais4").css("font-size", "45px")
+                }
+            )
+
+
+            $(".pais5").click(
+                function() { 
+                
+                    $(".pais5").css("background", "blue")
+                }
+                )
+                
+                $(".cambio5").dblclick(
+                    function() { 
+                    
+                        $(".pais5").css("font-size", "47px")
+                    }
+                )
+
+                $(".pais6").click(
+                    function() { 
+                    
+          $(".pais6").css("background", "pink")
+         }
+        )
+                    
+        $(".cambio6").dblclick(
+        function() { 
+                        
+         $(".pais6").css("font-size", "29px")
+         }
+          )
+
+
+                    $(".pais7").click(
+                        function() { 
+                        
+                            $(".pais7").css("background", "red")
+                        }
+                        )
+                        
+                        $(".cambio7").dblclick(
+                            function() { 
+                            
+                                $(".pais7").css("font-size", "46px")
+                            }
+                        )
+
+                        $(".pais8").click(
+                            function() { 
+                            
+                                $(".pais8").css("background", "brown")
+                            }
+                            )
+                            
+                            $(".cambio8").dblclick(
+                                function() { 
+                                
+                                    $(".pais8").css("font-size", "47px")
+                                }
+                            )
+
+                 $(".pais9").click(
+                 function() { 
+                                
+               $(".pais9").css("background", "green")
+    }
+        )
+                                
+  $(".cambio9").dblclick(
+    function() { 
+                                    
+     $(".pais9").css("font-size", "50px")
+     }
+)
+      $(".pais10").click(
+         function() { 
+                                    
+        $(".pais10").css("background", "black")
+        }
+          )
+                                    
+     $(".cambio10").dblclick(
+     function() { 
+                                        
+     $(".pais10").css("font-size", "55px")
+     } 
+      )
+
+$(".Caja1").mouseover(
+    function() { 
+    
+        $(".Caja1").css("background", "yellow")
+    }
+    )
+
+    $(".Caja2").mouseover(
+        function() { 
+        
+            $(".Caja2").css("background", "orange")
+        }
+        )
+
+        $(".Caja3").mouseover(
+            function() { 
+            
+                $(".Caja3").css("background", "green")
+            }
+            )
+    
+                     
+                        
+
